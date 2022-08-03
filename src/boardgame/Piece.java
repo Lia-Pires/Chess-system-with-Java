274ffb2208d6fbebdf02c0c3a61d9346e099c235
Lia Pires <liasth.pires@gmail.com>
2022-08-03 15:58:06 -0300

    Create ChessMatch, ChessPiece and UI Classes
    Create ChessMatch, ChessPiece and UI Classes

--- src/boardgame/Piece.java
+++ src/boardgame/Piece.java
@@ -2,4 +2,36 @@ package boardgame;
 
 public class Piece {
 
+		protected Position position;
+		private Board board;
+		
+		
+		public Piece() {
+			
+		}
+		
+		public Piece(Board board) {
+			this.board = board;
+			position = null;  // Already is null by standard;
+		}
+		
+			
+		protected Board getBoard() {
+			return board;
+		}
+
+		
+//		public boolean possibleMoves() {
+//			
+//		}
+//		
+//		public boolean possibleMove(Position position) {
+//			
+//		}
+//		
+//		public boolean isThereAnyPossibleMove() {
+//			
+//		}
+		
+		
 }
---
+++ src/chess/ChessPiece.java
@@ -0,0 +1,41 @@
+package chess;
+
+import boardgame.Board;
+import boardgame.Piece;
+
+public class ChessPiece extends Piece {
+
+		private Color color;
+//		private int moveCount;
+		
+		
+		
+		public ChessPiece(Board board, Color color) {
+			super(board);
+			this.color = color;
+		}
+
+		public Color getColor() {
+			return color;
+		}
+		
+		
+
+//		private ChessPosition getChessPosition() {
+//			
+//		}
+//		
+//		protected boolean isThereOpponentPiece(Position position) {
+//			
+//		}
+//		
+//		protected void increaseMoveCount() {
+//			
+//		}
+//		
+//		protected void decreaseMoveCount() {
+//			
+//		}
+		
+	
+}
