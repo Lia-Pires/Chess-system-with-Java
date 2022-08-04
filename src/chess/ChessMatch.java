5b0c0acc3c3c08191ab1a396706fe681d1cc505d
Lia Pires <liasth.pires@gmail.com>
2022-08-04 17:52:07 -0300

    Adding King and Rook
    Adding King and Rook

--- src/chess/ChessMatch.java
+++ src/chess/ChessMatch.java
@@ -1,6 +1,8 @@
 package chess;
 
 import boardgame.Board;
+import boardgame.Position;
+import chess.pieces.Rook;
 
 public class ChessMatch {
 	
@@ -15,7 +17,8 @@ public class ChessMatch {
 	
 	
 	public ChessMatch() {
-		board = new Board(8, 8);		
+		board = new Board(8, 8);
+		initialSetup();
 	}
 	
 	public ChessPiece[][] getPieces(){
@@ -29,7 +32,9 @@ public class ChessMatch {
 		return mat;
 	}
 	
-	
+	private void initialSetup() {
+		board.placePiece(new Rook(board, Color.WHITE), new Position(2,1));
+	}
 	
 	
 	
